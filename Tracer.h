9816ba0309a93e6ed0c30cb79c70eab600d11945
jaime <jcernudagarcia@hawk.iit.edu>
2024-01-29 17:28:54 +0100

    expanded to argument printing

--- Tracer.h
+++ Tracer.h
@@ -10,89 +10,164 @@
 #include <thread>
 #include <string>
 #include <sstream>
+#include <unistd.h>
 
-#ifdef MPI_VERSION
-#include <mpi.h>
+#ifdef __GNUG__
+#include <cstdlib>
+#include <memory>
+#include <cxxabi.h>
 
-#define INIT_TRACE_MANAGER() \
-    static int _traceRank;   \
-    MPI_Comm_rank(MPI_COMM_WORLD, &_traceRank); \
-    TraceManager traceManager(_traceRank);
-
-#define TRACE_FUNC() \
-    static int _traceRank;   \
-    MPI_Comm_rank(MPI_COMM_WORLD, &_traceRank); \
-    TraceLogger traceLogger(__func__, _traceRank)
-
+std::string demangle(const char* name) {
+  int status = -1;
+  std::unique_ptr<char, void(*)(void*)> res{
+      abi::__cxa_demangle(name, NULL, NULL, &status),
+      std::free
+  };
+  return (status == 0) ? res.get() : name;
+}
 #else
-#define INIT_TRACE_MANAGER(_traceRank) \
-    TraceManager traceManager(_traceRank);
-
-#define TRACE_FUNC(_traceRank) \
-    TraceLogger traceLogger(__func__, _traceRank)
+std::string demangle(const char* name) {
+    return name;
+}
 #endif
 
+#define TRACE_FUNC TraceLogger traceLogger(__func__)
+
+
+template<typename T>
+class EasySingleton {
+ protected:
+  /** static instance. */
+  static T* obj_;
+  static std::mutex lock_;
+
+ public:
+  /**
+   * Uses unique pointer to build a static global instance of variable.
+   * @tparam T
+   * @return instance of T
+   */
+  template<typename ...Args>
+  static T* GetInstance(Args&& ...args) {
+    if (obj_ == nullptr) {
+      std::scoped_lock lock(lock_);
+      if (obj_ == nullptr) {
+        obj_ = new T(std::forward<Args>(args)...);
+      }
+    }
+    return obj_;
+  }
+};
+template <typename T>
+T* EasySingleton<T>::obj_ = nullptr;
+template <typename T>
+std::mutex EasySingleton<T>::lock_ = std::mutex();
 
 class TraceManager {
-public:
-    explicit TraceManager(int rank){
-        std::string fileName = "trace_" + std::to_string(rank) + ".json";
-        spdlog::basic_logger_mt("trace_logger", fileName);
+ public:
+  explicit TraceManager(int rank) {
+    std::string fileName = "trace_" + std::to_string(rank) + ".json";
+    spdlog::basic_logger_mt("trace_logger", fileName);
 
-        auto log = spdlog::get("trace_logger");
-        log->set_pattern(entryPattern);
-        log->info("");
-        log->set_pattern(jsonPattern);
-    }
+    auto log = spdlog::get("trace_logger");
+    log->set_pattern(entryPattern);
+    log->info("");
+    log->set_pattern(jsonPattern);
+  }
 
-    ~TraceManager() {
-        auto log = spdlog::get("trace_logger");
-        log->set_pattern(exitPattern);
-        log->info("");
-        spdlog::drop("trace_logger");
-    }
+  ~TraceManager() {
+    auto log = spdlog::get("trace_logger");
+    log->set_pattern(exitPattern);
+    log->info("");
+    spdlog::drop("trace_logger");
+  }
 
-private:
-    std::string entryPattern = {"["};
-    std::string exitPattern = {"]"};
-    std::string jsonPattern = {"{%v},"};
+ private:
+  std::string entryPattern = {"["};
+  std::string exitPattern = {"]"};
+  std::string jsonPattern = {"{%v},"};
 };
 
 class TraceLogger {
-public:
-    TraceLogger(std::string functionName, int rank)
-            : functionName_(std::move(functionName)), rank_(rank) {
-        logEvent("B"); // "B" for Begin
+ public:
+  template<typename... Args>
+  explicit TraceLogger(std::string functionName, Args &&... args)
+      : functionName_(std::move(functionName)) {
+        auto logClient = EasySingleton<TraceManager>::GetInstance();
+    logEvent("B", std::forward<Args>(args)...); // "B" for Begin
+  }
+
+  ~TraceLogger() {
+    logEvent("E"); // "E" for End
+  }
+
+ private:
+  std::string functionName_;
+
+  static auto getTime(){
+    auto now = std::chrono::system_clock::now();
+    auto duration_since_epoch = now.time_since_epoch();
+    return std::chrono::duration_cast<std::chrono::microseconds>(duration_since_epoch).count();
+  }
+
+  static auto getTID(){
+    std::stringstream ss;
+    ss << std::this_thread::get_id();
+    return ss.str();
+  }
+
+  template<typename T, typename = void>
+  struct is_streamable : std::false_type {};
+
+  template<typename T>
+  struct is_streamable<T, std::void_t<decltype(std::declval<std::ostream&>() << std::declval<T>())>> : std::true_type {};
+
+  template<typename T>
+  std::string stringifyArg(const T& arg) {
+    if constexpr (is_streamable<T>::value) {
+      std::ostringstream ss;
+      ss << arg;
+      return ss.str();
+    } else {
+      return "[non-streamable type]";
+    }
+  }
+
+  template<typename... Args>
+  std::string logArguments(Args&&... args) {
+    std::ostringstream ss;
+    ss << "{\n";
+    int unpack[] = {0, (ss << "  \"" << demangle(typeid(Args).name()) << "\": " << stringifyArg(args) << ",\n", 0)...};
+    static_cast<void>(unpack); // To avoid unused variable warning
+    std::string result = ss.str();
+    if (sizeof...(Args) > 0) {
+      // Remove the last comma
+      result.erase(result.rfind(','), 1);
+    }
+    result += "}";
+    return result;
+  }
+
+  template<typename... Args>
+  void logEvent(const std::string &phase, Args &&... args) {
+    auto microseconds = getTime();
+    auto tid = getTID();
+
+    std::string logEntry = "\"name\": \"" + functionName_
+        + "\", \"ph\": \"" + phase
+        + "\", \"ts\": " + std::to_string(microseconds)
+        + ", \"pid\": " + std::to_string(getpid())
+        + ", \"tid\": " + tid;
+
+    if constexpr (sizeof...(args) != 0) {
+      std::string argsJson = logArguments(std::forward<Args>(args)...);
+      logEntry += ", \"args\":" + argsJson;
     }
 
-    ~TraceLogger() {
-        logEvent("E"); // "E" for End
-    }
-
-private:
-    std::string functionName_;
-    int rank_;
-
-    void logEvent(const std::string &phase) {
-        auto now = std::chrono::system_clock::now();
-        auto duration_since_epoch = now.time_since_epoch();
-        auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(duration_since_epoch).count();
-        std::stringstream ss;
-        ss << std::this_thread::get_id();
-        std::string threadID = ss.str();
-
-        std::string logEntry = "\"name\": \"" + functionName_
-                               + "\", \"ph\": \"" + phase
-                               + "\", \"ts\": " + std::to_string(microseconds)
-                               + ", \"pid\": " + std::to_string(rank_)
-                               + ", \"tid\": " + threadID
-                               + "";
-
-        auto log = spdlog::get("trace_logger");
-        log->info(logEntry);
-    }
+    auto log = spdlog::get("trace_logger");
+    log->info(logEntry);
+  }
 };
-
 
 #endif //DISTRIBUTEDTRACER_TRACER_H
 
